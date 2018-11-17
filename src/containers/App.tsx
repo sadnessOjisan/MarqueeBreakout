fc3668fa057e898d36e42b921384ee43912e5e97
sadness_ojisan <sadness.ojisan@gmail.com>
2018-11-17 20:55:40 +0900

    update: get scroll position

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,20 +1,52 @@
 import * as React from "react";
-import styled from "styled-components";
-import Component from '../components/Component'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Text>hellooohh!</Text>
-        <Component />
-      </div>
-    );
+declare global {
+  namespace JSX {
+      interface IntrinsicElements {
+          'marquee': any;
+      }
   }
 }
 
-const Text = styled.p`
-  color: red;
-`;
+interface Props {
+
+}
+
+class App extends React.Component<Props, any> {
+  private text = React.createRef<HTMLParagraphElement>();
+  constructor(props:Props) {
+    super(props);
+    this.text = React.createRef();
+    this.state = {
+      position: null
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      console.log(this.text.current);
+      console.log(this.text.current);
+      const left = this.text.current && this.text.current.getBoundingClientRect().left;
+      this.setState({
+        position: left
+      });
+    }, 100);
+  }
+
+  render() {
+    const {
+      position
+    } = this.state;
+    return ( <div className = "App" >
+      <h1> Hello CodeSandbox </h1>
+      <h2 > Start editing to see some magic happen! </h2>
+      <marquee behavior = "alternate" scrollamount = "30" >
+      <span ref = {this.text} >--------------------</span></marquee >
+      left: {
+        position
+      } </div>
+    );
+  }
+}
 
 export default App;
